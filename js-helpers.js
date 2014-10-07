61969e85f0227a25f8868dd553e7cbee6063dac3
Ethan Miller  - Development Server <ethanzanemiller@gmail.com>
2014-10-07 11:27:37 +0200

    prototype augmentation method added; array reduce funciton added

--- js-helpers.js
+++ js-helpers.js
@@ -1,4 +1,14 @@
 /**
+ * Augments the object on which it is called with the specified method.
+ * @function
+ * @returns {Object} The prototype object of the object that has been augmented.
+ */
+Function.prototype.method = function(name, func) {
+	this.prototype[name] = func;
+	return this;
+}
+
+/**
  * Identifies whether a variable is an array or not.
  * @function
  * @param {*} value The variable to be checked
@@ -7,3 +17,14 @@
 var is_array = function(value) {
 	return Object.prototype.toString.apply(value === '[object Array]';
 }
+
+/**
+ * Allows user to do computation on arrays.
+ * @function
+ */
+Array.method('reduce', function(f, value) {
+	var i;
+	for (i = 0; i< this.length; i += 1) {
+		value = f(this[i], value);
+	}
+});
